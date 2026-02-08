740ee0701f5f754b25842b561b5819d1f8e0fc96
Emilio Quezada <emilioquezada608@gmail.com>
2026-02-07 22:44:07 -0400

    test_modbus primero

---
+++ testmodbus.py
@@ -0,0 +1,39 @@
+from pymodbus.client import ModbusTcpClient
+import time
+
+IP = "192.168.47.242"
+PORT = "502"
+COIL_LED = 0
+
+client = ModbusTcpClient(IP, port=PORT)
+
+if not client.connect():
+    print("No hay conexión")
+    exit()
+
+print("Conexión establecida")
+
+try:
+    while True:
+        opcion = input("1 para encender | 0 para apagar | q para salir")
+        if opcion.lower() == "q":
+            break
+        
+        if opcion not in ["0","1"]:
+            print("No ingresaste una opción valida")
+            continue
+
+        valor = bool(int(opcion))
+        client.write_coil(COIL_LED, valor)
+
+        estado = "Encendido" if valor else "Apagado"
+        print(f"El estado es {estado}")
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    client.close()
+    print("Programa finalizado")
